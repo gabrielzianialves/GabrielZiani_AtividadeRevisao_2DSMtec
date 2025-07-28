d04a387e2cff7a8760a60b05857f2588b7f2ae0f
GABRIEL ZIANI ALVES <gabriel.alves381@etec.sp.gov.br>
2025-07-28 09:47:20 -0300

    first commit

---
+++ GeradorExtratos.java
@@ -0,0 +1,6 @@
+public class GeradorExtratos {
+    public void geradorConta(Conta conta) {
+        System.out.println("Saldo atual: " + conta.getSaldo());
+        System.out.println("Operações realizadas:");
+    }
+}
